9372a6191ba68f1805d49f48eb40f6f2fedf9b9c
Ayomide-Oyeneye <ayomidx1@gmail.com>
2023-10-19 17:47:52 -0700

    update

--- src/routes/homepage.js
+++ src/routes/homepage.js
@@ -4,7 +4,14 @@ function Homepage() {
   return (
     <>
       <h1 className="homepage">Welcome to my home page</h1>
-      <p>This project is a simple React application that demonstrates the creation of a custom counter hook with functions for increment, decrement, reset, and setting a value. It also includes a user interface for the counter. Additionally, it implements routing for different pages, including a custom counter page, a 404 page for handling unknown routes, and a page to test error boundary handling. This project serves as a basic example of managing state and navigation in a React application.</p>
+      <p style={{textAlign:'center'}}>
+        This project is a simple React application that demonstrates the
+        creation of a custom counter hook with functions for increment,
+        decrement, reset, and setting a value. It also includes a user interface
+        for the counter. Additionally, it implements routing for different
+        pages, including a custom counter page, a 404 page for handling unknown
+        routes, and a page to test error boundary handling.{" "}
+      </p>
     </>
   );
 }
